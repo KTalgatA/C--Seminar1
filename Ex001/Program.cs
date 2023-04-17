3928a6b6ae36b732153f9fb7e5339cfab6d472c5
«Talgat» <tkabysh@gmail.com>
2023-04-18 04:45:57 +0600

    Example0001 Complete

---
+++ Ex001/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число a: ");
+string userInputa = Console.ReadLine() ?? "";
+int userNumbera = int.Parse(userInputa);
+Console.Write("Введите число b: ");
+string userInputb = Console.ReadLine() ?? "";
+int userNumberb = int.Parse(userInputb);
+
+if (userNumbera/userNumberb == userNumberb)
+  {
+    Console.Write($"a = {userNumbera} b = {userNumberb} -> Yes");
+  }
+else
+  {
+    Console.Write($"a = {userNumbera} b = {userNumberb} -> No");
+  }
