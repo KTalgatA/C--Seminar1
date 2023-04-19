ddfdb3b409b192a377ec7dd8e183f8cfb49e97dd
«Talgat» <tkabysh@gmail.com>
2023-04-19 19:15:55 +0600

    Структурирование репозиториев

---
+++ Ex000/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+int result = userNumber * userNumber;
+Console.WriteLine($"{userNumber} -> {result}");
--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -5,11 +5,11 @@ Console.Write("Введите число b: ");
 string userInputb = Console.ReadLine() ?? "";
 int userNumberb = int.Parse(userInputb);
 
-if (userNumbera/userNumberb == userNumberb)
-  {
-    Console.Write($"a = {userNumbera} b = {userNumberb} -> Yes");
-  }
+if (userNumbera / userNumberb == userNumberb)
+{
+  Console.Write($"a = {userNumbera} b = {userNumberb} -> Yes");
+}
 else
-  {
-    Console.Write($"a = {userNumbera} b = {userNumberb} -> No");
-  }
+{
+  Console.Write($"a = {userNumbera} b = {userNumberb} -> No");
+}
