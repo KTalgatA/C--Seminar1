a6ce93a3a44c8ff8d4890c1710da198ac58e72aa
«Talgat» <tkabysh@gmail.com>
2023-04-20 00:42:04 +0600

    Example 4 Complete

---
+++ Ex004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine() ?? "");
+if (a < 100)
+{
+  Console.Write("Ошибка, необходимо ввести трехзначное число!!!");
+}
+else if (a > 999)
+{
+  Console.Write("Ошибка, необходимо ввести трехзначное число!!!");
+}
+int result = a - a / 10 * 10;
+Console.WriteLine($" {a} -> {result} ");
