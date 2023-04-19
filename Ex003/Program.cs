8608380452fa68f23923f0886c793ac8623a1dec
«Talgat» <tkabysh@gmail.com>
2023-04-20 00:12:36 +0600

    Example 0003 complete

---
+++ Ex003/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите целое число: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Все целые числа в промежутке от -N до N: ");
+for (int i = -N; i <= N; i++)
+{
+  Console.Write(i + " ");
+}
