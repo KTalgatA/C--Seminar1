25aed4e0cc790af898f252480116f84483210d83
«Talgat» <tkabysh@gmail.com>
2023-04-20 01:52:58 +0600

    Home Work 3 is Complete

---
+++ Ex007(HW_3)/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите целое число: ");
+int a = int.Parse(Console.ReadLine() ?? "");
+if (a % 2 == 0)
+{
+  Console.WriteLine($" {a} -> Да");
+}
+else
+{
+  Console.WriteLine($" {a} -> Нет");
+}
