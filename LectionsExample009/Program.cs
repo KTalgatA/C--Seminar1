018f446bde2e8a2c24b34db645b31c740ce52195
«Talgat» <tkabysh@gmail.com>
2023-04-26 18:58:19 +0600

    Лекция, пример 2

---
+++ LectionsExample009/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 12, 31, 34, 53, 64, 37, 28 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+  }
+  // index = index + 1;
+  index++;
+}
